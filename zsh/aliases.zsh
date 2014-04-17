e5de5869f1c04beabc6571eef6b48e4cc5c7a5b4
Oscar Carrero <osccar@gmail.com>
2014-04-17 10:47:21 +0200

    New aliases added
    - default editor to edit aliases is mvim
    - nginx, php-fpm and mysql aliases for localhost development

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,13 +1,14 @@
 # ------------------------------------------------------------------------------
 #          FILE:  aliases.zsh
 #   DESCRIPTION:  my zsh aliases
-#        AUTHOR:  Oscar Carrero (osccar@gmail.com)
-#       VERSION:  1.0
-#       DATE:     January 29, 2014
+#        AUTHOR:  Oscar Carrero
+#          DATE:  March 12, 2014
 # ------------------------------------------------------------------------------
 
 # System utilities
+#
 alias ll='ls -lahF'
+alias lt='ll -t'
 alias du='du -hc'
 alias .=pwd
 alias reload='exec $SHELL'
@@ -19,14 +20,17 @@ alias cpr='cp -Riv'
 alias hh='history'
 
 # Aliases
-alias aliases="cat $HOME/.dotfiles/zsh/aliases.zsh"
-alias aliases-open="nano HOME/.dotfiles/zsh/aliases.zsh"
+#
+alias aliases="less $HOME/.dotfiles/zsh/aliases.zsh"
+alias aliases-open="mvim HOME/.dotfiles/zsh/aliases.zsh"
 
 # Super user
+#
 alias _='sudo'
 alias please='sudo'
 
 # Show history
+#
 if [ "$HIST_STAMPS" = "mm/dd/yyyy" ]
 then
     alias history='fc -fl 1'
@@ -41,11 +45,29 @@ else
 fi
 
 # Fix multiple versions of apps in 'Open with...' menu in Finder
+#
 alias fixopenwithdups='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user; killall Finder;'
 
-# Homebrew MySql
-#alias mysql.up='mysql.server start'
-#alias mysql.down='mysql.server stop'
+# NGINX
+#
+alias nginx.start='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist'
+alias nginx.stop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist'
+alias nginx.reload='nginx -s reload'
+alias nginx.restart='nginx.stop && nginx.start'
+alias nginx.logs.error='tail -250f /usr/local/var/log/nginx/error.log'
+alias nginx.logs.access='tail -250f /usr/local/var/log/nginx/access.log'
+alias nginx.logs.debug='tail -250f /usr/local/var/log/nginx/error-debug.log'
 
-# Ruby commands
-#alias rspec='rspec --color --format doc'
+# PHP-FPM
+#
+alias php-fpm.start="launchctl load -w ~/Library/LaunchAgents/homebrew-php.josegonzalez.php55.plist"
+alias php-fpm.stop="launchctl unload -w ~/Library/LaunchAgents/homebrew-php.josegonzalez.php55.plist"
+alias php-fpm.restart='php-fpm.stop && php-fpm.start'
+alias php-fpm.log='tail -200f /usr/local/var/log/php-fpm.log'
+
+# MySQL
+#
+alias mysql.start="launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist"
+alias mysql.stop="launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist"
+alias mysql.restart='mysql.stop && mysql.start'
+alias mysql.status='mysql.server status'
